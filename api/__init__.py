e352d680aa9a8ffd8fe34e0f2b4f8df004bd8afa
Manukyan Gor <gmanuky@gmail.com>
2022-04-06 16:44:13 +0200

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_graphql import GraphQLView
 from api.graphql.schema import schema
-
-app = Flask(__name__)
+from flask_cors import CORS
 
 
 def create_app():
     app = Flask(__name__)
-
+    CORS(app)
+    
     app.add_url_rule(
         '/graphql',
         view_func=GraphQLView.as_view(
